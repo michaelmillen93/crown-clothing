9c5805107d7d9ed6a1b4accdea051c0188cc13ce
Michael Millen <mmillen16@gmail.com>
2021-07-19 11:18:35 -0400

    upddated stripe integration, favicon, and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JExmUHx7GrRHdIuo4G49EXhNIu912gHa6evL1PIqeJe7qOijJMPcAjHnsmkJLFO6s29174mmRM133smcdCr2fTC00vQxKSuFj";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN CLOTHING LTD."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
